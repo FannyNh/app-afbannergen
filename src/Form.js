0c708a6d997f0d36172cd4c20cfafcc53b8b741a
thethird <thethirdmoira@gmail.com>
2020-03-21 16:37:47 +0100

    refacto + exporthtmlfile

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Transform from './Transform';
-
+import html from './template_banner/FR/160x600/index.html';
 
 
 
@@ -12,41 +12,92 @@ class Form extends React.Component {
       slogan1: '',
       cta: '',
       campagnName: '',
+      // export: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.stringsChanger = this.stringsChanger.bind(this);
+    this.exportFiles = this.exportFiles.bind(this);
+    this.downloadTxtFile = this.downloadTxtFile.bind(this);
+  }
+  //methode
+  stringsChanger = (props) => {
+    var htmlToReplace = props;
+    var newhtml = htmlToReplace.replace("HEADLINE__replace", this.state.head);
+    return newhtml;
+  }
+  exportFiles = (props) => {
+
+    //export
+    console.log('yolo');
+    this.downloadTxtFile();
+    // this.setState({ export: false });
+
+  }
+  downloadTxtFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([this.stringsChanger(html)], { type: 'text/plain' });
+    element.href = URL.createObjectURL(file);
+    element.download = "index.html";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  }
+  zipFiles = () =>{
+    
   }
   handleChange = (event) => {
-    if (event.target.name === "headline") { this.setState({ headline: event.target.value }); }
-    if (event.target.name === "slogan1") { this.setState({ slogan1: event.target.value }); }
+    if (event.target.name === "headline") {
+      if (true) { }
+      if (true) { }
+      else {
+        this.setState({ headline: event.target.value });
+
+      }
+
+    }
+    if (event.target.name === "slogan1") {
+
+      this.setState({ slogan1: event.target.value });
+
+    }
     if (event.target.name === "cta") { this.setState({ cta: event.target.value }); }
     if (event.target.name === "campagnName") { this.setState({ campagnName: event.target.value }); }
   }
 
   handleSubmit(event) {
+    // this.setState({ export: true }); 
+    this.exportFiles();
     event.preventDefault();
   }
+
+
+
   render() {
 
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
           <label > headline : </label>
-          <input type="text" id="headline" name="headline" onChange={this.handleChange}/>
+          <input type="text" id="headline" name="headline" onChange={this.handleChange} />
           <label > slogan1 : </label>
           <input type="text" name="slogan1" id="slogan1" onChange={this.handleChange} />
           <label > cta : </label>
-          <input type="text" name="cta" id="cta"  onChange={this.handleChange}/>
+          <input type="text" name="cta" id="cta" onChange={this.handleChange} />
           <label > campagnName : </label>
           <input type="text" name="campagnName" id="campagnName" onChange={this.handleChange} />
 
-          <input type="submit" value="Envoyer" />
+          <input type="submit" value="Envoyer" disabled={false} />
         </form>
+        {this.stringsChanger(html)}
 
-        <Transform head={this.state.headline} slogan1={this.state.slogan1} cta={this.state.cta} />
+        {/* <Transform
+            head={this.state.headline}
+            slogan1={this.state.slogan1}
+            cta={this.state.cta}
+            export="false" /> */}
       </div>
 
-    );
+    )
 
   }
 
--- src/Transform.js
+++ src/Transform.js
@@ -6,6 +6,8 @@ class Transform extends React.Component{
     constructor(props){
     super(props);
     this.stringsChanger=this.stringsChanger.bind(this);
+    this.exportFiles=this.exportFiles.bind(this);
+    this.downloadTxtFile=this.downloadTxtFile.bind(this);
     }
     //methode
     stringsChanger = (props)=>{
@@ -13,6 +15,22 @@ class Transform extends React.Component{
         var newhtml = htmlToReplace.replace("HEADLINE__replace", this.props.head);
         return newhtml;
     }
+    exportFiles = (props)=>{
+        if(this.props.export){
+            //export
+            console.log('yolo');
+            this.downloadTxtFile();
+            this.setState({ export: false });
+        }
+    }
+    downloadTxtFile = () => {
+        const element = document.createElement("a");
+        const file = new Blob( [this.stringsChanger(html)], {type: 'text/plain'});
+        element.href = URL.createObjectURL(file);
+        element.download = "index.html";
+        document.body.appendChild(element); // Required for this to work in FireFox
+        element.click();
+      }
     render(){
         
         // return null;
@@ -20,7 +38,7 @@ class Transform extends React.Component{
             <div>
               {this.stringsChanger(html)}
             </div>
-        );
+        ) && this.exportFiles()
     }
 }
 
